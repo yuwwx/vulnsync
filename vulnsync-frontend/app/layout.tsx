f0e5d5f844b22e376a5a89f56d6aee8aed2522d4
mac-air <mac-air@Bobrovko-VV-Work.local>
2026-01-22 20:27:28 +0300

    add fonts

--- vulnsync-frontend/app/layout.tsx
+++ vulnsync-frontend/app/layout.tsx
@@ -1,19 +1,141 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+export const inter = localFont({
+  variable: "--font-sans",
+  src: [
+    { path: "/fonts/inter/Inter-Thin.woff2", weight: "100", style: "normal" },
+    {
+      path: "/fonts/inter/Inter-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    { path: "/fonts/inter/Inter-Light.woff2", weight: "300", style: "normal" },
+    {
+      path: "/fonts/inter/Inter-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    { path: "/fonts/inter/Inter-Medium.woff2", weight: "500", style: "normal" },
+    {
+      path: "/fonts/inter/Inter-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    { path: "/fonts/inter/Inter-Bold.woff2", weight: "700", style: "normal" },
+    {
+      path: "/fonts/inter/Inter-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    { path: "/fonts/inter/Inter-Black.woff2", weight: "900", style: "normal" },
+  ],
 });
 
-const geistMono = Geist_Mono({
+export const geistSans = localFont({
+  variable: "--font-geist-sans",
+  src: [
+    {
+      path: "/fonts/geist/Geist-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist/Geist-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
+
+export const geistMono = localFont({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  src: [
+    {
+      path: "/fonts/geist-mono/GeistMono-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "/fonts/geist-mono/GeistMono-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
