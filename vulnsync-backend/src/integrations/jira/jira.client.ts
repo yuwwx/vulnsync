245f62ae46f9f3d5f6f0c58ed6d29ff7113bc3de
mac-air <mac-air@Bobrovko-VV-Work.local>
2026-02-03 11:22:29 +0300

    add jira error handling

--- vulnsync-backend/src/integrations/jira/jira.client.ts
+++ vulnsync-backend/src/integrations/jira/jira.client.ts
@@ -1,12 +1,18 @@
 // jira.client.ts
 import axios, { AxiosInstance } from 'axios';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 import { IntegrationType } from '@/common/enums/integration-type.enum';
 import https from 'https';
 
 @Injectable()
 export class JiraClient {
+  private readonly logger = new Logger(JiraClient.name);
+
   constructor(private prisma: PrismaService) {}
 
   private async getClient(): Promise<AxiosInstance> {
@@ -35,27 +41,68 @@ export class JiraClient {
   }
 
   async createIssue(payload: any) {
-    const client = await this.getClient();
-    //const { data } = await client.post('/rest/api/2/issue', payload);
-    const { data } = await client.post('/', payload);
-    return data;
+    try {
+      const client = await this.getClient();
+      const { data } = await client.post('/rest/api/2/issue', payload);
+      return data;
+    } catch (error) {
+      this.logger.error(
+        'Jira createIssue failed',
+        error.response?.data || error.message,
+      );
+
+      throw new InternalServerErrorException(
+        'Jira API error while creating issue',
+      );
+    }
   }
 
   async getIssue(key: string) {
-    const client = await this.getClient();
-    const { data } = await client.get(`/rest/api/2/issue/${key}`);
-    return data;
+    try {
+      const client = await this.getClient();
+      const { data } = await client.get(`/rest/api/2/issue/${key}`);
+      return data;
+    } catch (error) {
+      this.logger.error(
+        'Jira createIssue failed',
+        error.response?.data || error.message,
+      );
+
+      throw new InternalServerErrorException('Jira API error while get issue');
+    }
   }
 
   async getProjects() {
-    const client = await this.getClient();
-    const { data } = await client.get('/rest/api/2/project');
-    return data;
+    try {
+      const client = await this.getClient();
+      const { data } = await client.get('/rest/api/2/project');
+      return data;
+    } catch (error) {
+      this.logger.error(
+        'Jira createIssue failed',
+        error.response?.data || error.message,
+      );
+
+      throw new InternalServerErrorException(
+        'Jira API error while get projects',
+      );
+    }
   }
 
   async getCustomFields() {
-    const client = await this.getClient();
-    const { data } = await client.get('/rest/api/2/field');
-    return data;
+    try {
+      const client = await this.getClient();
+      const { data } = await client.get('/rest/api/2/field');
+      return data;
+    } catch (error) {
+      this.logger.error(
+        'Jira createIssue failed',
+        error.response?.data || error.message,
+      );
+
+      throw new InternalServerErrorException(
+        'Jira API error while custom fields',
+      );
+    }
   }
 }
